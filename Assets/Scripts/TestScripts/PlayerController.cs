2fcc2572aaf04fd1b855551810e0c93b0e2ac399
Crami <crami.andrada@gmail.com>
2022-02-17 12:33:31 +0100

    Doble salto, planeo y coyote time

--- Assets/Scripts/TestScripts/PlayerController.cs
+++ Assets/Scripts/TestScripts/PlayerController.cs
@@ -6,27 +6,34 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField]
-    private float playerSpeed;
+    private Camera followCamera;
+    [SerializeField]
+    GameObject Paraguas;
 
+    [SerializeField]
+    private float playerSpeed;
     [SerializeField]
     private float rotationSpeed;
-
-    [SerializeField]
-    private Camera followCamera;
-
-    
-
     [SerializeField]
     private float jumpHeight;
     [SerializeField]
+    private float doubleJumpHeight;
+    [SerializeField]
     private float gravityValue;
+    [SerializeField]
+    private float gravityOnFloor;
+    [SerializeField]
+    private float gravityGliding;
+    [SerializeField]
+    private float coyoteTime;
 
-
+    
 
     private float horizontalInput = 0;
     private float verticalInput = 0;
     private bool groundedPlayer;
-
+    private bool doubleJumped = false;
+    private bool canCoyote;
     private Vector3 playerVelocity;
     private Vector3 movementInput;
     private Vector3 movementDirection;
@@ -37,32 +44,47 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         controller = GetComponent<CharacterController>();
+
     }
 
     private void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-        
-        
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
         Jump();
         MovePlayer();
-        RotatePlayer();
-        
     }
 
     void MovePlayer()
     {
-        
-        movementInput = Quaternion.Euler(0, followCamera.transform.eulerAngles.y, 0) * new Vector3(horizontalInput, 0, verticalInput);
+        if (groundedPlayer)
+        {
+            //movimiento en el suelo
+            movementInput = Quaternion.Euler(0, followCamera.transform.eulerAngles.y, 0) * new Vector3(horizontalInput, 0, verticalInput);
+        }
+        else
+        {
+            //definir movimiento en el aire
+            if (horizontalInput != 0 || verticalInput != 0)
+            {
+                movementInput = Quaternion.Euler(0, followCamera.transform.eulerAngles.y, 0) * new Vector3(horizontalInput, 0, verticalInput);
+            }
+        }
+        // Se coge la direccion en la que va a moverse el personaje en base a los inputs pulsados y los 
         movementDirection = movementInput.normalized;
 
         controller.Move(movementDirection * playerSpeed * Time.deltaTime);
-       
 
+        RotatePlayer();
+
+
+        // aplica la gravedad
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
-
     }
 
     void RotatePlayer() {
@@ -82,12 +104,60 @@ public class PlayerController : MonoBehaviour
         {
             //Si el player esta en el suelo esto hace que no se caiga por la gravedad
             playerVelocity.y = 0f;
+            doubleJumped = false;
+            gravityValue = gravityOnFloor;
+            Paraguas.SetActive(false);
+            canCoyote = true;
+
         }
 
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        if (!groundedPlayer && canCoyote)
         {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            StartCoroutine(WaitForCoyoteTime());
         }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+
+            if (groundedPlayer || canCoyote)
+            {
+                playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+                canCoyote = false;
+            }
+            else if (!doubleJumped)
+            {
+                playerVelocity.y = 0;
+                playerVelocity.y += Mathf.Sqrt(doubleJumpHeight * -3.0f * gravityValue);
+                doubleJumped = true;
+            }
+            
+        }
+
+        if (Input.GetButton("Jump"))
+        {
+
+            //Si el boton de saltar esta presionado 
+            if (playerVelocity.y < 0)
+            {
+                Paraguas.SetActive(true);
+                gravityValue = gravityGliding;
+            }
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            gravityValue = gravityOnFloor;
+            Paraguas.SetActive(false);
+        }
+
+
+    }
+
+
+    IEnumerator WaitForCoyoteTime() {
+
+        yield return new WaitForSeconds(coyoteTime);
+        canCoyote = false;
 
     }
 
