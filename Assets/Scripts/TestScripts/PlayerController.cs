3dfef393452e55d5e084e3adfccf71d6b18ab012
Crami <crami.andrada@gmail.com>
2022-02-20 20:48:57 +0100

    Prototipo air controll y comentarios
    He hecho un prototipo de lo que podria ser un air controll agregando cambios de aceleracion dependiendo de si esta en el suelo y en el aire, máxima velocidad de caída y gran parte del texto comentado

--- Assets/Scripts/TestScripts/PlayerController.cs
+++ Assets/Scripts/TestScripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    
+
 
     //inputs
     private float horizontalInput = 0;
@@ -13,38 +13,77 @@ public class PlayerController : MonoBehaviour
     private Vector3 movementInput;
     private Vector3 movementDirection;
 
-    //Player Speeds
+    [Header("Player Movment")]
     [SerializeField]
+    [Tooltip("Esta es la velocidad base que tendra el personaje")]
     private float playerSpeed;
     [SerializeField]
+    [Tooltip("Velocidad en la que el personaje gira a los lados")]
     private float rotationSpeed;
+    [Tooltip("Vector de velocidad del personaje (seria como el rigidbody.velocity pero para el character controller)")]
     private Vector3 playerVelocity;
-    private float currentSpeed;
+    [Tooltip("Saber si el personaje se esta pulsando algun input de movimiento , esto se usa para la aceleracion o la deceleracion")]
     private bool isAccelerating = false;
+    [Tooltip("Variable que contiene el valor actual del multiplicador para hacer la aceleracion y deceleracion ")]
+    private float currentSpeed;
     [SerializeField]
-    private float accelSpeed;
+    [Tooltip("Velocidad en la que el personaje acelera estando en el suelo")]
+    private float floorAccel;
     [SerializeField]
-    private float dragSpeed;
+    [Tooltip("Velocidad en la que el personaje decelera en el suelo")]
+    private float floorBraking;
+    [SerializeField]
+    [Tooltip("Velocidad en la que el personaje acelera estando en el aire")]
+    private float airAccel;
+    [SerializeField]
+    [Tooltip("Velocidad en la que el personaje decelera en el aire")]
+    private float airBraking;
+    [Tooltip("Este es el multiplicador de velocidad y aceleracion el cual ira cambiando segun el estado en el que este (corriendo saltando ...)")]
+    private float currentStateSpeed;
+    [SerializeField]
+    [Tooltip("Valor al que va a ajustarse el multiplicador de velocidad mientras anda")]
+    private float walkAccelSpeed;
+    [SerializeField]
+    [Tooltip("Valor al que va a ajustarse el multiplicador de velocidad mientras corre")]
+    private float runAccelSpeed;
+    [SerializeField]
+    [Tooltip("Valor al que va a ajustarse el multiplicador de velocidad mientras esta volando")]
+    private float airAccelSpeed;
 
-    //Jump
+    [Header("Jump")]
     [SerializeField]
+    [Tooltip("Altura que tendra el salto base estando en el suelo")]
     private float jumpHeight;
     [SerializeField]
+    [Tooltip("Altura del doble salto")]
     private float doubleJumpHeight;
     [SerializeField]
+    [Tooltip("Valor con el que se haran los calculos de la gravedad")]
     private float gravityValue;
     [SerializeField]
+    [Tooltip("Valor de la gravedad al estar en el suelo o en el aire")]
     private float gravityOnFloor;
     [SerializeField]
+    [Tooltip("Valor de la gravedad al estar planeando")]
     private float gravityGliding;
+    [Tooltip("Maxima velocidad de en la que puede caer el personaje")]
+    private float maxFallSpeed;
     [SerializeField]
+    [Tooltip("Maxima velocidad de caida")]
+    private float normalFallSpeed;
+    [SerializeField]
+    [Tooltip("Maxima velocidad de caida mientras se planea")]
+    private float glidingFallSpeed;
+
+    [SerializeField]
+    [Tooltip("El tiempo que dispondra el player para saltar y no tener que hacer el salto pixel perfect")]
     private float coyoteTime;
     private bool groundedPlayer;
     private bool doubleJumped = false;
     private bool canCoyote;
 
 
-    //Components
+    [Header("Components & External objects")]
     [SerializeField]
     private Camera followCamera;
     [SerializeField]
@@ -55,6 +94,9 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         controller = GetComponent<CharacterController>();
+        //Setear la velocidad inicial
+        currentStateSpeed = walkAccelSpeed;
+        maxFallSpeed = normalFallSpeed;
 
     }
 
@@ -66,6 +108,7 @@ public class PlayerController : MonoBehaviour
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
 
+        CheckIfRunning();
         CheckInputSpeed();
         Jump();
         MovePlayer();
@@ -84,18 +127,27 @@ public class PlayerController : MonoBehaviour
             if (horizontalInput != 0 || verticalInput != 0)
             {
                 movementInput = Quaternion.Euler(0, followCamera.transform.eulerAngles.y, 0) * new Vector3(horizontalInput, 0, verticalInput);
+                currentStateSpeed = airAccelSpeed; 
             }
         }
         // Se coge la direccion en la que va a moverse el personaje en base a los inputs pulsados y los 
         movementDirection = movementInput.normalized;
-
+        // Se le indica al controller que se mueva hacia la direccion indicada y ajustandolo a la velocidad que le queramos aplicar
         controller.Move(movementDirection * playerSpeed * currentSpeed * Time.deltaTime);
 
         RotatePlayer();
 
 
-        // aplica la gravedad
-        playerVelocity.y += gravityValue * Time.deltaTime;
+        // Se aplica la gravedad que le pasemos pero que no supere limite de velocidad de caida
+        if (playerVelocity.y > maxFallSpeed)
+        {
+            playerVelocity.y += gravityValue * Time.deltaTime;
+        }
+        else
+        {
+            playerVelocity.y = maxFallSpeed;
+        }
+        Debug.Log(playerVelocity.y);
         controller.Move(playerVelocity * Time.deltaTime);
     }
 
@@ -115,17 +167,18 @@ public class PlayerController : MonoBehaviour
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
-            //Si el player esta en el suelo esto hace que no se caiga por la gravedad
+            //Si el player esta en el suelo esto hace que no se caiga por la gravedad y reseteamos los valores del salto (numer de saltos, coyote time ...)
             playerVelocity.y = 0f;
             doubleJumped = false;
             gravityValue = gravityOnFloor;
             Paraguas.SetActive(false);
             canCoyote = true;
-
+            maxFallSpeed = normalFallSpeed;
         }
 
         if (!groundedPlayer && canCoyote)
         {
+            // Si no esta tocando el suelo espera el tiempo indicado par que aun pueda hacer el primer salto
             StartCoroutine(WaitForCoyoteTime());
         }
 
@@ -134,12 +187,14 @@ public class PlayerController : MonoBehaviour
 
             if (groundedPlayer || canCoyote)
             {
+                // En caso de que este en el suelo o aun este a tiempo de utilizar el coyote time haz el 1r salto
                 playerVelocity.y = 0;
                 playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
                 canCoyote = false;
             }
             else if (!doubleJumped)
             {
+                // En caso de que no haya hecho el doble salto que haga el doble salto
                 playerVelocity.y = 0;
                 playerVelocity.y += Mathf.Sqrt(doubleJumpHeight * -3.0f * gravityValue);
                 doubleJumped = true;
@@ -153,48 +208,105 @@ public class PlayerController : MonoBehaviour
             //Si el boton de saltar esta presionado 
             if (playerVelocity.y < 0)
             {
+                //En el momento en el que el personaje deje de subir empezara a planear
                 Paraguas.SetActive(true);
                 gravityValue = gravityGliding;
+                maxFallSpeed = glidingFallSpeed;
             }
         }
 
         if (Input.GetButtonUp("Jump"))
         {
+            // Si deja de apretar el boton de salto que deje de planear
             gravityValue = gravityOnFloor;
             Paraguas.SetActive(false);
+            maxFallSpeed = normalFallSpeed;
         }
 
 
     }
 
+    private void CheckIfRunning() {
+
+        // Revisar segun si ha apretado el boton de correr o no empezara a correr o dejara de hacerlo
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            currentStateSpeed = runAccelSpeed;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            currentStateSpeed = walkAccelSpeed;
+
+        }
+    }
+
     private void CheckInputSpeed() {
 
+        // En esta funcion se revisara la velocidad a la que tiene que ir el personaje segun su estado
 
         if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
         {
+            // En caso de que este presionando algun imput contara como que esta acelerando
             isAccelerating = true;
         }
         else
         {
+            // En caso de que no presione ningun input contara como que esta decelerando
             isAccelerating = false;
         }
 
+
         if (isAccelerating)
         {
-            if (currentSpeed < 1)
+
+            //En caso de que esta acelerando revisaremos si tiene que hacelerar o frenar para ajustar el multiplicador al valor del estado actual
+            if (groundedPlayer)
             {
-                currentSpeed += accelSpeed/1000;
+                // Si estamos en el suelo
+                if (currentSpeed < currentStateSpeed)
+                {
+                    // Aceleraremos con la velocidad de aceleracion en el suelo
+                    currentSpeed += floorAccel / 1000;
+                }
+                else if(currentSpeed > currentStateSpeed)
+                {
+                    // Frenaremos con la velocidad de frenar en el suelo
+                    currentSpeed -= floorBraking / 1000;
+                }
+                else
+                {
+                    
+                    currentSpeed = currentStateSpeed;
+                }
+
             }
             else
             {
-                currentSpeed = 1;
+                if (currentSpeed > currentStateSpeed)
+                {
+                    currentSpeed -= airBraking / 1000;
+                }
+                else
+                {
+                    currentSpeed = currentStateSpeed;
+                }
             }
+
+
         }
         else
         {
             if (currentSpeed > 0)
             {
-                currentSpeed -= dragSpeed/1000;
+                if (groundedPlayer)
+                {
+                    currentSpeed -= floorBraking / 1000;
+                }
+                else
+                {
+                    currentSpeed -= airBraking / 1000;
+                }
 
             }
             else
