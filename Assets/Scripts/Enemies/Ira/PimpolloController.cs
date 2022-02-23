d3e5621ecf43370d484a2665359a9bf33929e7a3
Crami <crami.andrada@gmail.com>
2022-02-23 17:47:37 +0100

    Prototipo de enemigo 1 terminado

--- Assets/Scripts/Enemies/Ira/PimpolloController.cs
+++ Assets/Scripts/Enemies/Ira/PimpolloController.cs
@@ -4,23 +4,84 @@ using UnityEngine;
 using UnityEngine.AI;
 public class PimpolloController : MonoBehaviour
 {
-    private NavMeshAgent navMesh;
 
     [SerializeField]
-    private Vector3[] PlaceToGo;
+    private float timeToWait;
+    [SerializeField]
+    private float distanceToAttack;
+    [SerializeField]
+    private float attackSpeed;
+
+    private float attackTranscurse = 0;
+    private float attackDuration = 0.2f;
+    private bool chasePlayer = false;
+    private bool isAttacking = false;
+    private NavMeshAgent navMesh;
+    private GameObject player;
+    private BoxCollider boxColl;
+
     // Start is called before the first frame update
     void Start()
     {
         navMesh = GetComponent<NavMeshAgent>();
-
-        navMesh.destination = PlaceToGo[0];
-
+        boxColl = GetComponent<BoxCollider>();
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (chasePlayer)
+        {
+            if (Vector3.Distance(transform.position, player.transform.position) > distanceToAttack && !isAttacking)
+            {
+                //Debug.Log(Vector3.Distance(transform.position, player.transform.position));
+                navMesh.destination = player.transform.position;
+
+            }
+            else
+            {
+                isAttacking = true;
+                navMesh.enabled = false;
+                if (attackTranscurse < attackDuration)
+                {
+                    attackTranscurse += attackSpeed * Time.deltaTime;
+                }
+                else
+                {
+                    attackTranscurse = 1;
+                }
+
+                transform.position = Vector3.Lerp(transform.position, player.transform.position, attackTranscurse);
+
+                if (attackTranscurse >= 1)
+                {
+                    Destroy(gameObject);
+                }
+
+                Debug.Log(attackTranscurse);
+            }
+
+
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            player = other.gameObject;
+            StartCoroutine(PlayerSeen());
+        }
+    }
+
+
+    IEnumerator PlayerSeen() {
+
+        boxColl.enabled = false;
+        transform.LookAt(player.transform);
+        yield return new WaitForSeconds(timeToWait);
+        chasePlayer = true;
     }
 }
