ee82066ce2015c4b4699354ea399346ee9911cd8
Pedro Fernandez <pedro.fernandez@enti.cat>
2022-03-09 18:08:48 +0100

    Particulas de Saltar y Doble salto hechas

--- Assets/Scripts/Beleth/ParticleController.cs
+++ Assets/Scripts/Beleth/ParticleController.cs
@@ -4,25 +4,59 @@ using UnityEngine;
 
 public class ParticleController : MonoBehaviour
 {
-
-    [SerializeField] ParticleSystem KirbyDust;
-    [SerializeField] ParticleSystem MarioDust;
+    [Header("Walk")]
+    [SerializeField] ParticleSystem WalkDust;
     [SerializeField] Transform LeftAnkle;
     [SerializeField] Transform RightAnkle;
     [SerializeField] Transform Ankles;
 
-    public void ActivateLeftDustFBX()
+    [Header("Run")]
+    [SerializeField] ParticleSystem KirbyDust;
+    [SerializeField] ParticleSystem MarioDust;
+
+    [Header("Jump")]
+    [SerializeField] ParticleSystem ExteriorJumpDust;
+    [SerializeField] ParticleSystem InteriorJumpDust;
+    [SerializeField] ParticleSystem JumpTrailDust;
+    [SerializeField] Transform Socket;
+
+    public void WalkLeftDustFbx()
+    {
+        //Instantiate(WalkDust, LeftAnkle.position, LeftAnkle.rotation);
+    }
+
+    public void WalkRightDustFbx()
+    {
+        //Instantiate(WalkDust, RightAnkle.position, RightAnkle.rotation);
+    }
+
+    public void RunLeftDustFbx()
     {
         Instantiate(KirbyDust, LeftAnkle.position, LeftAnkle.rotation); 
     }
     
-    public void ActivateRightDustFBX()
+    public void RunRightDustFbx()
     {
         Instantiate(KirbyDust, RightAnkle.position, RightAnkle.rotation); 
     }
     
-    public void ActivateDustFBX()
+    public void RunDustFbx()
     {
         Instantiate(MarioDust, Ankles.position, Ankles.rotation); 
+    }
+
+    public void JumpDustFbx()
+    {
+        Instantiate(ExteriorJumpDust, Ankles.position, Ankles.rotation);
+    }
+    
+    public void DobleJumpDustFbx()
+    {
+        Instantiate(InteriorJumpDust, new Vector3(Ankles.position.x, Ankles.position.y + 1f, Ankles.position.z), Ankles.rotation);
+    }
+
+    public void JumpTrailDustFbx()
+    {
+
     }
 }
