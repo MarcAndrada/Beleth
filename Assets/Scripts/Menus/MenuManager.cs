8b219417d397181a72b41f080d0d06af54496aab
Pedro <pedro.fernandez@enti.cat>
2022-03-28 13:45:58 +0200

    Splash Screen, Main Menu y Pause

---
+++ Assets/Scripts/Menus/MenuManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("Play")]
+    [SerializeField]
+    string level;
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
