db76fae1f5eee4a52254313b28bb5a2f8827a0ce
DanielMoncayo <rcmoncayodaniel@gmail.com>
2022-03-30 18:13:27 +0200

    Canvas Video Tutorial y 2 videos de prueba

---
+++ Assets/TutorialCanvasController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class TutorialCanvasController : MonoBehaviour
+{
+
+    [SerializeField]
+    VideoPlayer videoPlayer;
+    [SerializeField]
+    double timesToPlay;
+    double timeToStop;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeToStop = videoPlayer.length * timesToPlay;
+        timer = 0;
+        
+    }
+
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer>= timeToStop)
+        {
+            timer = 0;
+            gameObject.SetActive(false);
+        }
+    }
+
+   
+
+    public void SetLenght()
+    {
+
+        timeToStop = videoPlayer.length * timesToPlay;
+    }
+}
