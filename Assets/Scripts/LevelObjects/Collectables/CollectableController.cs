54010c64bf511257945cdfc3c24f0a4a06624af8
Pedro Fernandez <pedro.fernandez@enti.cat>
2022-04-27 19:29:16 +0200

    Efecto Fantasma y Coleccionable del Favor
    Estan en mi escena, me he marcado un Daniel.

--- Assets/Scripts/LevelObjects/Collectables/CollectableController.cs
+++ Assets/Scripts/LevelObjects/Collectables/CollectableController.cs
@@ -7,16 +7,47 @@ public class CollectableController : MonoBehaviour
     public int collectableID;
     [SerializeField]
     private GameObject breakObject;
+    [SerializeField]
+    private GameObject firstVfx;
+    [SerializeField]
+    private GameObject secondVfx;
 
     private SphereCollider coll;
+    private bool isDone;
+
+    private float t;
+
     private void Awake()
     {
         coll = GetComponent<SphereCollider>();
+        firstVfx.SetActive(false);
+    }
+
+    private void Start()
+    {
+        isDone = false;
+        t = 0;
+    }
+
+    private void Update()
+    {
+        if (isDone)
+        {
+            t += Time.deltaTime;
+        }
+
+        if (t > 4)
+        {
+            Instantiate(secondVfx, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
     }
 
     public void DisableCollectable() 
     {
+        firstVfx.SetActive(true);
         coll.enabled = false;
         breakObject.SetActive(false);
+        isDone = true;
     }
 }
